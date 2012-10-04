62271001e6824bb20178ae04dfd2f7131c934f46
piotras <piotrek.pokora@gmail.com>
2012-10-04 14:27:55 +0200

    Short docs.

--- raki/WorkspaceManager.php
+++ raki/WorkspaceManager.php
@@ -2,8 +2,26 @@
 
 interface WorkspaceManager {
 
+    /**
+     * Returns array of possible workspaces names.
+     *
+     * Every key is workspace name and value is an array which holds
+     * child workspaces.
+     */ 
     public function getPossibleWorkspacesNames($parent = null);
+
+    /**
+     * Returns array of all possible workspaces paths.
+     * 
+     */ 
     public function dumpWorkspaces();
+
+    /**
+     * Create MidgardWorskpace.
+     *
+     * No exception should be thrown if workspace with given name and 
+     * under given parent workspace already exists.
+     */ 
     public function createWorkspace($name, $parent = null);
 }
 
