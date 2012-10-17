9212bf295bf0eba26206ce1448f5e7dd330b6ca9
piotras <piotrek.pokora@gmail.com>
2012-10-17 08:52:14 +0200

    Initial

---
+++ tests/00_Workspace/001_StorableWorkspace.php
@@ -0,0 +1,37 @@
+<?php
+
+class StorableWorkspaceTest extends RakiTest 
+{
+    private $manager = null;
+    private $midgardWorkspaceManager = null;
+
+    public function setUp() 
+    {
+        $this->manager = new RagnaroekWorkspaceManager();
+        $this->midgardWorkspaceManager = new midgard_workspace_manager(MidgardConnection::get_instance());
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals("TODO", "DONE");
+    }
+
+    public function testGePath()
+    {
+        $this->assertEquals("TODO", "DONE");
+    }
+
+    public function testGetWorkspacesNames()
+    {
+        $this->assertEquals("TODO", "DONE");
+    }
+
+    public function testGetWorkspaces()
+    {
+        $this->assertEquals("TODO", "DONE");
+    }
+
+}
+
+?>
+
