2558ca7da568f576d3308042cc423a269ef8c662
piotras <piotrek.pokora@gmail.com>
2012-10-25 12:32:55 +0200

    Initial

---
+++ raki/Ragnaroek/StorableWorkspace.php
@@ -0,0 +1,43 @@
+<?php
+
+class RagnaroekStorableWorkspace implements StorableWorkspace
+{
+    private $workspace = null;
+
+    public function __construct(MidgardWorkspace $workspace)
+    {
+        $this->workspace = $workspace;
+    }
+
+    public function getName()
+    {
+        return $this->workspace->name;
+    }
+
+    public function getPath()
+    {
+        return $this->workspace->path;
+    }
+
+    public function getChildrenNames()
+    {
+        return $this->workspace->list_workspace_names();
+    }
+
+    public function getChildren()
+    {
+        $children = $this->workspace->list_children();
+        if (empty($children)) {
+            return array();
+        }
+
+        $ret = array();
+        foreach ($children as $child) {
+            $ret[] = new RagnaroekStorableWorkspace($child);
+        }
+
+        return $ret;
+    }
+}
+
+?>
