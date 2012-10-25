3c1ad2422532ef330226b7b6f25288cfbe34f6c9
piotras <piotrek.pokora@gmail.com>
2012-10-25 13:58:19 +0200

    Remove MidgardWorkspaceManager

--- tests/00_Workspace/001_StorableWorkspace.php
+++ tests/00_Workspace/001_StorableWorkspace.php
@@ -3,12 +3,10 @@
 class StorableWorkspaceTest extends RakiTest 
 {
     private $manager = null;
-    private $midgardWorkspaceManager = null;
 
     public function setUp() 
     {
         $this->manager = new RagnaroekWorkspaceManager();
-        $this->midgardWorkspaceManager = new midgard_workspace_manager(MidgardConnection::get_instance());
     }
 
     public function testGetName()
