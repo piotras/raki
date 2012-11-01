3db1e62ddc482f7a52158563be12b7bf89ae44eb
piotras <piotrek.pokora@gmail.com>
2012-11-01 12:22:28 +0100

    Initial

---
+++ raki/Ragnaroek/Transition.php
@@ -0,0 +1,39 @@
+<?php
+
+class RagnaroekTransition implements Transition
+{
+    private $mgd = null;
+    private $fixtureDir = null;
+    private $contentManager = null;
+    private $workspaceManager = null;
+    
+    public function __construct(MidgardConnection $mgd, $fixtureDir)
+    {
+        $this->mgd = $mgd;
+    } 
+
+    public function getContentManager()
+    {
+        if ($this->contentManager == null) {
+            $this->contentManager = new RagnaroekContentManager($this);
+        }
+
+        return $this->contentManager;
+    }
+
+    public function getWorkspaceManager()
+    {
+        if ($this->workspaceManager == null) {
+            $this->workspaceManager == new RagnaroekWorkspaceManager($this);
+        }
+
+        return $this->workspaceManager;
+    }
+
+    public function getResultFixtureDirPath()
+    {
+        return $this->fixtureDir;
+    } 
+}
+
+?>  
