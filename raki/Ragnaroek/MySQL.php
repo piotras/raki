4a1188df2682c09807e6358ffd469c1c11612945
piotras <piotrek.pokora@gmail.com>
2012-11-08 13:00:29 +0100

    Initial

---
+++ raki/Ragnaroek/MySQL.php
@@ -0,0 +1,31 @@
+<?php
+
+class RagnaroekMySQL 
+{
+    private $cnc = null;
+    private $result = null;
+
+    public function __construct($host, $dbname, $dbuser, $dbpass)
+    {
+        $this->cnc = mysql_connect($host, $dbuser, $dbpass)
+            or die('Could not connect to host: ' . mysql_error());
+
+       mysql_select_db($dbname) or die("Could not select {$dbname} database"); 
+    }
+
+    public function query($sql)
+    {
+        if ($this->result != null) {
+            mysql_free_result($this->result);
+        }
+
+        $this->result = mysql_query($sql) or die('Query failed: ' . mysql_error());
+    }
+
+    public function getQueryResult()
+    {
+        return mysql_fetch_array($this->result); 
+    }
+}
+
+?>
