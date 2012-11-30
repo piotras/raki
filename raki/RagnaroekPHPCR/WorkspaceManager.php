d3e93ce87dd6a6a3f95422772891d569c6123d80
piotras <piotrek.pokora@gmail.com>
2012-11-30 13:55:53 +0100

    Gest sitegroups and workspaces.

--- raki/RagnaroekPHPCR/WorkspaceManager.php
+++ raki/RagnaroekPHPCR/WorkspaceManager.php
@@ -4,6 +4,8 @@ class RagnaroekPHPCRWorkspaceManager implements WorkspaceManager
 {
     private $transition = null;
     private $defaultWorkspaceName = null;
+    private $sitegroups = array();
+    private $possibleWorkspaces = null;
 
     public function __construct($transition, $workspaceName = "")
     {
@@ -23,37 +25,98 @@ class RagnaroekPHPCRWorkspaceManager implements WorkspaceManager
 
     public function getStorableTypeNames()
     {
-        throw new Exception("Not implemented");
+        $re = new ReflectionExtension("midgard2");
+        $names = array();
+        foreach ($re->getClasses() as $class_ref) {
+            $class_mgd_ref = new midgard_reflection_class($class_ref->getName());
+            $name = $class_mgd_ref->getName();
+            /* Ignore non MidgardDBObject derived types */
+            if (!is_subclass_of ($name, 'MidgardDBObject')) {
+                continue;
+            }
+            /* Ignore interfaces, abstract and mixin types */
+            if (MidgardObjectReflector::is_abstract($name) 
+                || MidgardObjectReflector::is_mixin($name)
+                || MidgardObjectReflector::is_interface($name)) {
+                    continue;
+                }
+
+            $names[] = $name;
+        }
+        return $names;
+    }
+
+    private function populateSitegroups()
+    {
+        if (!empty($this->sitegroups)) {
+            return $this->sitegroups;
+        }
+
+        $storage = new MidgardQueryStorage("ragnaroek_sitegroup");
+        $qs = new MidgardQuerySelect($storage);
+        $qs->execute();
+
+        if ($qs->get_results_count() < 1) {
+            return;
+        }
+
+        foreach ($qs->list_objects() as $sg) {
+            $this->sitegroups[] = $sg;
+        }
     }
 
     public function getPossibleWorkspacesNames()
     {
-        throw new Exception("Not implemented");
+        $this->populateSitegroups();
+        $names = array();
+
+        foreach ($this->sitegroups as $sg) {
+            $names[] = $sg->name;
+        }    
+
+        return $names;
     }
 
     public function getPossibleWorkspacesPaths()
     {
-        throw new Exception("Not implemented");
+        $names = $this->getPossibleWorkspacesNames();
+        $paths = array();
+        foreach ($names as $name) {
+            $paths[] = "/" . $name;
+        }
+
+        return $paths;
     }
 
     public function createWorkspace($name, StorableWorkspace $parent = null)
     {
-        throw new Exception("Not implemented");
+        $phpcrSession = $this->getTransition()->getSession();
+        $phpcrSession->getWorkspace()->createWorkspace($name);
     }
 
     public function createWorkspacesAll()
     {
-        throw new Exception("Not implemented");
+        $names = $this->getPossibleWorkspacesNames();
+        foreach ($names as $name) {
+            $this->createWorkspace($name);
+        }
     }
 
     public function getStoredWorkspacesNames()
     {
-        throw new Exception("Not implemented");
+        return $this->getTransition()->getSession->getWorkspace()->getAccessibleWorkspaceNames();
     }
 
     public function getStoredWorkspacesPaths()
     {
-        throw new Exception("Not implemented");
+        $names = $this->getStoredWorkspacesNames();
+        $paths = array();
+        if (!empty($names)) {
+            foreach ($names as $name) {
+                $paths[] = "/" . $name;
+            }
+        }
+        return $paths;      
     }
 
     public function getStoredWorkspaceByPath($absPath)
