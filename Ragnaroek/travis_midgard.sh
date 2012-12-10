67b68558423f6ef8563a27302301dd3058520d70
piotras <piotrek.pokora@gmail.com>
2012-12-10 14:16:34 +0100

    Do not require pake

--- Ragnaroek/travis_midgard.sh
+++ Ragnaroek/travis_midgard.sh
@@ -4,10 +4,6 @@ if [ -n "$MIDGARD_EXT_VERSION:" ] ; then
 	MIDGARD_EXT_VERSION="master"
 fi
 
-# Install Pake
-pyrus channel-discover pear.indeyets.ru
-pyrus install -f http://pear.indeyets.ru/get/pake-1.6.3.tgz
-
 # Install Midgard2 library dependencies
 sudo apt-get install -y dbus libglib2.0-dev libgda-4.0-4 libgda-4.0-dev libxml2-dev libdbus-1-dev libdbus-glib-1-dev
 
@@ -20,5 +16,5 @@ rm -f ${MIDGARD_EXT_VERSION}
 # Build and install Midgard2 PHP extension
 wget -q https://github.com/midgardproject/midgard-php5/tarball/${MIDGARD_EXT_VERSION} -O ${MIDGARD_EXT_VERSION}
 tar zxf ${MIDGARD_EXT_VERSION}
-sh -c "cd midgardproject-midgard-php5-*&&php `pyrus get php_dir|tail -1`/pake.php install"
+sh -c "cd midgardproject-midgard-php5-*  && phpize && ./configure && sudo make install"
 echo "extension=midgard2.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
