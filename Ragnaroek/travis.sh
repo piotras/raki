04d342002b842aea8ee6ff3421836e8dd6b935ce
piotras <piotrek.pokora@gmail.com>
2012-12-12 13:34:21 +0100

    Try to execute test explicitly

--- Ragnaroek/travis.sh
+++ Ragnaroek/travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies with Composer
-wget -q http://getcomposer.org/composer.phar
-php composer.phar install --dev
+#wget -q http://getcomposer.org/composer.phar
+#php composer.phar install --dev
 
 php -c midgard2.ini `which phpunit` -c tests/phpunit.xml.dist
