e462a07ed2276f73cf5b6f3807bd89db7698acef
piotras <piotrek.pokora@gmail.com>
2012-12-13 13:50:44 +0100

    Simplify implementation

--- RagnaroekPHPCR/src/Ragnaroek/PHPCR/ContentManager.php
+++ RagnaroekPHPCR/src/Ragnaroek/PHPCR/ContentManager.php
@@ -2,31 +2,15 @@
 
 namespace Ragnaroek\PHPCR;
 
-use \MidgardReflectorProperty;
-use \MidgardReflectorObject;
-use \MidgardQueryStorage;
-use \MidgardQuerySelect;
-use \MidgardQueryConstraint;
-use \MidgardQueryProperty;
-use \MidgardQueryValue;
-
 class ContentManager implements \CRTransition\ContentManager 
 {
     private $transition = null;
-    private $xmlWriter = null;
+    private $sessions = null;
 
-    public function __construct($transition = null)
+    public function __construct($transition)
     {
         $this->transition = $transition;
-    }
-
-    public function getXmlWriter()
-    {
-        if ($this->xmlWriter === null) {
-            $this->xmlWriter = new XmlMidgardObjectWriter();
-        }
-
-        return $this->xmlWriter;
+        $this->sessions = array();
     }
 
     public function getTransition()
@@ -55,74 +39,27 @@ class ContentManager implements \CRTransition\ContentManager
         return $names;
     }
 
-    private function getObjects($typename, $upProperty = null, $upValue = 0)
+    private function getSessions()
     {
-        $qs = new MidgardQuerySelect(
-            new MidgardQueryStorage($typename)
-        );
-
-        if ($upProperty != null) {
-            $cnstr = new MidgardQueryConstraint(
-                new MidgardQueryProperty($upProperty),
-                "=", 
-                new MidgardQueryValue($upValue)
-            );
-            $qs->set_constraint($cnstr);
+        if (empty($this->sessions)) {
+            $workspaces = $this->getTransition()->getWorkspaceManager()->getPossibleWorkspacesNames();
+            foreach ($workspaces as $name) {
+                $this->sessions[$name] = $this->getTransition()->getPHPCRRepository()->login(null, $name);
+            }
         }
 
-        $qs->execute();
-        return $qs->list_objects();
-    }
-
-    private function serializeObjects($typeName, $upProperty, $upValue, $xmlParentNode)
-    {
-        $parentProperty = MidgardReflectorObject::get_property_parent($typeName);
-        $childrenTypes = MidgardReflectorObject::list_children($typeName);
-
-        /* Get all objects or root ones in case of tree */
-        $objects = $this->getObjects($typeName, $upProperty, $upValue);
-        foreach ($objects as $object) {
-            /* Create xml node from object */
-            $xmlNode = $this->getXmlWriter()->serializeObject($object);
-            /* Append node to document */
-            $xmlParentNode->appendChild($xmlNode);
-
-            /* Serialize possible children objects of the same type */
-            if ($upProperty != null) {
-                $this->serializeObjects($typeName, $upProperty, $object->id, $xmlNode);
-            }
-
-            /* Serialize possible children objects of different type */
-            if (!empty($childrenTypes)) {
-                foreach ($childrenTypes as $childType => $v) {
-                    $childParentProperty = MidgardReflectorObject::get_property_parent($childType);
-                    $this->serializeObjects($childType, $childParentProperty, $object->id, $xmlNode);
-                } 
-            }
-        }      
+        return $this->sessions();
     }
 
     public function importType($typeName)
     {
-        /* Determine if type can be parent or child in a tree.
-         * If parent property is defined, we have to create subgraph 
-         * while importing parent type */
-        $parentProperty = MidgardReflectorObject::get_property_parent($typeName);
-        if ($parentProperty != null) {
-            return;
+        $sessions = array();
+        $exportDir = $this->getTransition()->getExportDir();
+
+        foreach ($this->getSessions() as $name => $session) {
+            $filePath = $exportDir . "/" . $name . "/" . $typeName;
+            $session()->importXML("/", $filePath);
         }
-
-        $upProperty = MidgardReflectorObject::get_property_up($typeName);
-
-        $typeNode = $this->getXmlWriter()->addTypeNode($typeName);
-        $this->serializeObjects($typeName, $upProperty, 0, $typeNode);
-
-        /* Dump xml to a file */
-        $this->getXmlWriter()->save();
-
-        /* Import document using currrent session */
-        /* TODO, create new session if data is imported per sitegroup */
-        $this->getTransition()->getDefaultPHPCRSession()->importXML("/", $this->getXmlWriter()->getFilePath());
     }
 
     public function getStoredTypeNames()
