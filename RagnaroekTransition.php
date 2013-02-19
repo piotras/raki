8666bfcddc8937c4e49e029c1fe5bdce2adaf5c7
piotras <piotrek.pokora@gmail.com>
2013-02-19 11:00:42 +0100

    Initial

---
+++ RagnaroekTransition.php
@@ -0,0 +1,95 @@
+<?php
+
+# Database to transform
+$DB_LIVE_NAME='midgard';
+$DB_LIVE_USERNAME='midgard';
+$DB_LIVE_PASSWORD='midgard';
+$DB_LIVE_DUMP_FILE='midgard.sql';
+
+# Temporary database
+$DB_TMP_NAME='midgard_raki';
+$DB_TMP_USERNAME='midgard_raki';
+$DB_TMP_PASSWORD='midgard_raki';
+
+# Directory with schemas which hold information about types to transform
+$SCHEMA_LIVE_DIRECTORY='/usr/share/midgard/schema';
+
+# Directory for updated schemas (used for transition only)
+$SCHEMA_TMP_DIRECTORY='';
+
+# working directory
+$SRC_TOP_DIR = __DIR__;
+
+# Load mandatory files
+
+if (!extension_loaded('mysql')) {
+    throw new Exception('MySQL extension not loaded!');
+}
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . 'RagnaroekTransitionAbstract.php';
+
+# Create class
+
+class RagnaroekTransition extends RagnaroekTransitionAbstract
+{
+    public function __construct($db_live_name, $db_live_username, $db_live_password, $db_live_dump_file, $db_tmp_name, $db_tmp_username, $db_tmp_password, $schema_live_directory, $schema_tmp_directory, $scr_top_dir) 
+    {
+        $db_live_name = $db_live_name;
+	    $db_live_username = $db_live_username;
+	    $db_live_password = $db_live_password;
+	    $db_live_dump_file = $db_live_dump_file;
+
+	    $db_tmp_name = $db_tmp_name;
+	    $db_tmp_username = $db_tmp_username;
+	    $db_tmp_password = $db_tmp_password;
+
+	    $schema_live_directory = $schema_live_directory;
+	    $schema_tmp_directory = $schema_tmp_directory;
+
+	    $scr_top_dir = $scr_top_dir;
+    }
+
+    public function importContent()
+    {
+        $transition = new \Ragnaroek\Ratatoskr\Transition(MidgardConnection::get_instance(), $this->Midgard2Config, __DIR__ . '/fixtures/', __DIR__ . '/data/ragnaroek/schema');
+
+        $workspaceManager = $transition->getWorkspaceManager();
+        $workspaceManager->createWorkspacesAll();
+
+        $contentManager = $transition->getContentManager();
+        $types = $contentManager->getPossibleTypeNames();
+        foreach ($types as $type) {
+            echo "Import '{$type}' content \n";
+            $contentManager->importType($type);
+        }
+    }
+
+    public function execute()
+    {
+        $this->validate();
+        $this->prepareTransitionDatabase();
+        $this->dumpLiveDatabase();
+        $this->importDumpedDatabase();
+        $this->prepareMidgard2Connection();
+        $this->prepareMidgard2Storage();
+        $this->importContent();
+    }
+}
+
+# Run transition
+
+$transition = new RagnaroekTransition(
+	$DB_LIVE_NAME,
+    $DB_LIVE_USERNAME,
+    $DB_LIVE_PASSWORD,
+    $DB_LIVE_DUMP_FILE,
+    $DB_TMP_NAME,
+    $DB_TMP_USERNAME,
+    $DB_TMP_PASSWORD,
+    $SCHEMA_LIVE_DIRECTORY,
+    $SCHEMA_TMP_DIRECTORY,
+    $SRC_TOP_DIR
+    );
+$transition->execute();
+
+?>
---
+++ RagnaroekTransitionAbstract.php
@@ -0,0 +1,146 @@
+<?php
+
+abstract class RagnaroekTransitionAbstract
+{
+    protected $db_live_name = null;
+    protected $db_live_username = null;
+    protected $db_live_password = null;
+    protected $db_live_dump_file = null;
+
+    protected $db_tmp_name = null;
+    protected $db_tmp_username = null;
+    protected $db_tmp_password = null;
+
+    protected $schema_live_directory = null;
+    protected $schema_tmp_directory = null;
+
+    protected $scr_top_dir = null;
+
+    public function validate()
+    {
+        if ($db_live_name == null) {
+            throw new Exception("Invalid value");
+        }
+
+        if ($db_live_username == null) {
+            throw new Exception("Invalid value");
+        }
+
+        if ($db_live_password == null) {
+            throw new Exception("Invalid value");
+        }
+
+        if ($db_live_dump_file == null) {
+            throw new Exception("Invalid value");
+        }
+
+        if ($db_tmp_name == null) {
+            throw new Exception("Invalid value");
+        }
+
+        if ($db_tmp_username == null) {
+            throw new Exception("Invalid value");
+        }
+
+        if ($db_tmp_password == null) {
+            throw new Exception("Invalid value");
+        }
+
+        if ($schema_live_directory == null) {
+            throw new Exception("Invalid value");
+        }
+
+        if ($schema_tmp_directory == null) {
+            throw new Exception("Invalid value");
+        }
+    
+        if ($scr_top_dir == null) {
+            throw new Exception("Invalid value");
+        }
+    }
+
+    public function prepareTransitionDatabase()
+    {
+        echo "Create temporary database \n";
+        exec("sudo mysql -e 'CREATE DATABASE {$this->db_tmp_name} CHARACTER SET utf8'");
+        $cmdGrant = "GRANT all ON {$this->db_tmp_name}.*  to '{$this->db_tmp_username}'@'localhost' identified by '{$this->db_tmp_password}'";
+
+        echo "Grant all privileges \n";
+        exec("sudo mysql -e \"{$cmdGrant}\"");
+
+        echo "Flush privileges \n";
+        exec("sudo mysql -e 'FLUSH PRIVILEGES'");
+    }
+
+    public function dumpLiveDatabase()
+    {
+        echo "Dump live database \n";
+        $path = $this->src_top_dir . "/" . $this->db_live_dump_file;
+        $cmd = "sudo mysqldump -u {$this->db_live_username} -p{$this->db_live_password} {$this->db_live_name} > $path";
+        exec($cmd); 
+    }
+
+    public function importDumpedDatabase()
+    {
+        $path = $this->src_top_dir . "/" . $this->db_live_dump_file;
+        $testDB = $this->db_tmp_name;
+        echo "Import Sql dump {$path} \n";
+
+        exec("sudo mysql {$testDB} < {$path}", $out, $returnValue);
+
+        if ($returnValue != 0) {
+            throw new Exception("Failed to import data from sql file");
+        }
+    }
+
+    public function prepareMidgard2Connection()
+    {
+        $this->Midgard2Config = new MidgardConfig();
+        $this->Midgard2Config->dbtype = 'MySQL';
+        $this->Midgard2Config->database = $this->db_tmp_name;
+        $this->Midgard2Config->dbuser = $this->db_tmp_username;
+        $this->Midgard2Config->dbpass = $this->db_tmp_password;
+
+        $mgd = midgard_connection::get_instance();
+        $mgd->enable_workspace(true);
+        if ($mgd->open_config ($this->Midgard2Config) == false) {
+            throw new Exception("Failed to connect. " . $mgd->get_error_string());
+        }
+    }
+
+    public function prepareMidgard2Storage()
+    {
+        midgard_storage::create_base_storage();
+        $re = new ReflectionExtension("midgard2");
+        foreach ($re->getClasses() as $refClass) {
+            $name = $refClass->getName();
+            if (!is_subclass_of ($name, 'MidgardDBObject')
+                || $refClass->isAbstract()
+                || $refClass->isInterface()) {
+                    continue;
+                }
+
+            if (strpos($name, "_abstract") != false) {
+                continue;
+            }
+
+            echo 'midgard_storage: prepare class storage('.$name.")\n";
+            midgard_storage::create_class_storage($name);
+            midgard_storage::update_class_storage($name);
+        }
+    }
+
+    public function addSitegroupPropertyToSchema($file)
+    {
+        $tree = simplexml_load_file($file);
+        foreach ($tree->type as $k => $typedef)
+        {
+            $ch = $typedef->addChild('property');
+            $ch->addAttribute('name', 'sitegroup');
+            $ch->addAttribute('type', 'unsigned integer');
+        }
+        $tree->asXML($file);
+    }
+}
+
+?>
