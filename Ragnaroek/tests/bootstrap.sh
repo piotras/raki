7b1c53056126bdf90e4ebfb66f5236747c8ae246
piotras <piotrek.pokora@gmail.com>
2013-03-12 13:49:23 +0100

    We do not set default content with non existing file

--- Ragnaroek/tests/bootstrap.sh
+++ Ragnaroek/tests/bootstrap.sh
@@ -10,6 +10,3 @@ echo "Preparing test database"
 sudo mysql -e "CREATE DATABASE ${MIDGARD_TEST_DB} CHARACTER SET utf8";
 sudo mysql -e "GRANT all ON ${MIDGARD_TEST_DB}.*  to '${MIDGARD_TEST_DB_USER}'@'localhost' identified by '${MIDGARD_TEST_DB_PASS}'";
 sudo mysql -e " FLUSH PRIVILEGES";
-
-echo "Preparing default test content"
-php -c ./midgard2.ini ./ragnaroek_content_init.php
